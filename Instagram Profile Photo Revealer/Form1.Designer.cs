b1cc262ad2c80ebef5b3768c8fd2cd42429a587b
Dimitrios Zisis <32481990+dimizisis@users.noreply.github.com>
2018-04-17 15:29:57 +0300

    First Commit

---
+++ Instagram Profile Photo Revealer/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Instagram_Profile_Photo_Revealer
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.imageLink = new System.Windows.Forms.TextBox();
+            this.fixedLink = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.pasteBtn = new System.Windows.Forms.Button();
+            this.copyBtn = new System.Windows.Forms.Button();
+            this.browserBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // imageLink
+            // 
+            this.imageLink.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.imageLink.Location = new System.Drawing.Point(33, 12);
+            this.imageLink.Name = "imageLink";
+            this.imageLink.Size = new System.Drawing.Size(180, 20);
+            this.imageLink.TabIndex = 0;
+            this.imageLink.Text = "Enter image link...";
+            // 
+            // fixedLink
+            // 
+            this.fixedLink.Location = new System.Drawing.Point(33, 99);
+            this.fixedLink.Name = "fixedLink";
+            this.fixedLink.Size = new System.Drawing.Size(180, 20);
+            this.fixedLink.TabIndex = 1;
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.Location = new System.Drawing.Point(33, 51);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(104, 32);
+            this.generateBtn.TabIndex = 2;
+            this.generateBtn.Text = "Generate Link";
+            this.generateBtn.UseVisualStyleBackColor = true;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // pasteBtn
+            // 
+            this.pasteBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.pasteBtn.Location = new System.Drawing.Point(219, 12);
+            this.pasteBtn.Name = "pasteBtn";
+            this.pasteBtn.Size = new System.Drawing.Size(61, 20);
+            this.pasteBtn.TabIndex = 3;
+            this.pasteBtn.Text = "Paste";
+            this.pasteBtn.UseVisualStyleBackColor = true;
+            this.pasteBtn.Click += new System.EventHandler(this.pasteBtn_Click);
+            // 
+            // copyBtn
+            // 
+            this.copyBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.copyBtn.Location = new System.Drawing.Point(219, 99);
+            this.copyBtn.Name = "copyBtn";
+            this.copyBtn.Size = new System.Drawing.Size(61, 20);
+            this.copyBtn.TabIndex = 4;
+            this.copyBtn.Text = "Copy";
+            this.copyBtn.UseVisualStyleBackColor = true;
+            this.copyBtn.Click += new System.EventHandler(this.copyBtn_Click);
+            // 
+            // browserBtn
+            // 
+            this.browserBtn.Enabled = false;
+            this.browserBtn.Location = new System.Drawing.Point(152, 51);
+            this.browserBtn.Name = "browserBtn";
+            this.browserBtn.Size = new System.Drawing.Size(104, 32);
+            this.browserBtn.TabIndex = 5;
+            this.browserBtn.Text = "Open in Browser";
+            this.browserBtn.UseVisualStyleBackColor = true;
+            this.browserBtn.Click += new System.EventHandler(this.browserBtn_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 131);
+            this.Controls.Add(this.browserBtn);
+            this.Controls.Add(this.copyBtn);
+            this.Controls.Add(this.pasteBtn);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.fixedLink);
+            this.Controls.Add(this.imageLink);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(308, 170);
+            this.MinimumSize = new System.Drawing.Size(308, 170);
+            this.Name = "Main";
+            this.Text = "Instagram Profile Photo Revealer 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox imageLink;
+        private System.Windows.Forms.TextBox fixedLink;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.Button pasteBtn;
+        private System.Windows.Forms.Button copyBtn;
+        private System.Windows.Forms.Button browserBtn;
+    }
+}
+
---
+++ Instagram Profile Photo Revealer/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Instagram_Profile_Photo_Revealer
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+
+            if (!imageLink.Text.Contains("https:")) MessageBox.Show("Please enter a valid instagram photo link (Right Click On Image -> Open in New Window -> Copy Link)", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            else
+            {
+
+                string link = imageLink.Text; // Original image link
+
+                string startLink = "https://scontent-lga3-1.cdninstagram.com/"; // How fixed link will start!
+
+                string fLink = link.Replace("/vp", ""); // Retrieve last 113 characters of original link (the ones we need to place to our fixed)!
+
+                fixedLink.Text = startLink + fLink.Replace("s150x150", "s1080x1080"); // Start with fixed instagram server and place the ID's with new dimensions (1080x1080)!
+
+                browserBtn.Enabled = true; // Open in browser can now be clicked
+
+                fixedLink.SelectAll(); // Select all by double clicking on fixed link text
+
+            }
+
+        }
+
+        private void pasteBtn_Click(object sender, EventArgs e)
+        {
+            imageLink.Text = Clipboard.GetText(); // Paste from clipboard
+        }
+
+        private void copyBtn_Click(object sender, EventArgs e)
+        {
+            if (!fixedLink.Text.Equals(""))
+                Clipboard.SetText(fixedLink.Text); // Copy to clipboard
+        }
+
+        private void browserBtn_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(fixedLink.Text); // Open fixed link in default browser
+        }
+
+    }
+}
